b1743fa6cc13ad2bfc39afe085a99614510207ee
nkruger <nicolas.nkruger@gmail.com>
2024-06-27 15:10:04 -0300

    feat: :sparkles: ui

--- src/game/PhaserGame.tsx
+++ src/game/PhaserGame.tsx
@@ -1,86 +1,107 @@
-import { forwardRef, useEffect, useLayoutEffect, useRef } from 'react';
-import StartGame from './main';
-import { EventBus } from './EventBus';
+import {
+    forwardRef,
+    useEffect,
+    useLayoutEffect,
+    useRef,
+    useState,
+} from "react";
+import StartGame from "./main";
+import { EventBus } from "./EventBus";
+import styles from "./phaser.module.css";
+import { Game } from "./scenes/Game";
 
-export interface IRefPhaserGame
-{
+export interface IRefPhaserGame {
     game: Phaser.Game | null;
     scene: Phaser.Scene | null;
 }
 
-interface IProps
-{
-    currentActiveScene?: (scene_instance: Phaser.Scene) => void
+interface IProps {
+    currentActiveScene?: (scene_instance: Phaser.Scene) => void;
 }
 
-export const PhaserGame = forwardRef<IRefPhaserGame, IProps>(function PhaserGame({ currentActiveScene }, ref)
-{
-    const game = useRef<Phaser.Game | null>(null!);
+export const PhaserGame = forwardRef<IRefPhaserGame, IProps>(
+    function PhaserGame({ currentActiveScene }, ref) {
+        const game = useRef<Phaser.Game | null>(null!);
 
-    useLayoutEffect(() =>
-    {
-        if (game.current === null)
-        {
+        const [food, setFood] = useState(0);
+        const [endiabrado, setEndiabrado] = useState(0);
 
-            game.current = StartGame("game-container");
+        useEffect(() => {
+            const func = setInterval(() => {
+                if (typeof ref === "object") {
+                    const _game = ref?.current?.scene;
+                    if (_game instanceof Game) {
+                        setFood(_game.bat.food);
+                        setEndiabrado(_game.bat.endiabrado);
+                    }
+                }
+            }, 200);
+            return () => clearInterval(func);
+        }, []);
 
-            if (typeof ref === 'function')
-            {
-                ref({ game: game.current, scene: null });
-            } else if (ref)
-            {
-                ref.current = { game: game.current, scene: null };
-            }
+        useLayoutEffect(() => {
+            if (game.current === null) {
+                game.current = StartGame("game-container");
 
-        }
-
-        return () =>
-        {
-            if (game.current)
-            {
-                game.current.destroy(true);
-                if (game.current !== null)
-                {
-                    game.current = null;
+                if (typeof ref === "function") {
+                    ref({ game: game.current, scene: null });
+                } else if (ref) {
+                    ref.current = { game: game.current, scene: null };
                 }
             }
-        }
-    }, [ref]);
 
-    useEffect(() =>
-    {
-        EventBus.on('current-scene-ready', (scene_instance: Phaser.Scene) =>
-        {
-            if (currentActiveScene && typeof currentActiveScene === 'function')
-            {
+            return () => {
+                if (game.current) {
+                    game.current.destroy(true);
+                    if (game.current !== null) {
+                        game.current = null;
+                    }
+                }
+            };
+        }, [ref]);
 
-                currentActiveScene(scene_instance);
+        useEffect(() => {
+            EventBus.on(
+                "current-scene-ready",
+                (scene_instance: Phaser.Scene) => {
+                    if (
+                        currentActiveScene &&
+                        typeof currentActiveScene === "function"
+                    ) {
+                        currentActiveScene(scene_instance);
+                    }
 
-            }
+                    if (typeof ref === "function") {
+                        ref({ game: game.current, scene: scene_instance });
+                    } else if (ref) {
+                        ref.current = {
+                            game: game.current,
+                            scene: scene_instance,
+                        };
+                    }
+                }
+            );
+            return () => {
+                EventBus.removeListener("current-scene-ready");
+            };
+        }, [currentActiveScene, ref]);
 
-            if (typeof ref === 'function')
-            {
+        return (
+            <div id="game-container" className={styles.div}>
+                <div className={styles.div_div}>
+                    <span className={styles.span}>
+                        <strong>food:</strong> {food}
+                    </span>
+                    <span
+                        className={`${styles.span} ${
+                            endiabrado === 20 && styles.endiabrado
+                        }`}
+                    >
+                        <strong>endiabrado:</strong> {endiabrado}/20
+                    </span>
+                </div>
+            </div>
+        );
+    }
+);
 
-                ref({ game: game.current, scene: scene_instance });
-            
-            } else if (ref)
-            {
-
-                ref.current = { game: game.current, scene: scene_instance };
-
-            }
-            
-        });
-        return () =>
-        {
-
-            EventBus.removeListener('current-scene-ready');
-        
-        }
-    }, [currentActiveScene, ref]);
-
-    return (
-        <div id="game-container"></div>
-    );
-
-});

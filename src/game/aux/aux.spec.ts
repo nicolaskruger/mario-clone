02434080355e4f273cc1f4273f8ed287c3b6e798
nkruger <nicolas.nkruger@gmail.com>
2024-07-02 16:42:26 -0300

    test: :white_check_mark: iterate game map

--- src/game/aux/aux.spec.ts
+++ src/game/aux/aux.spec.ts
@@ -1,6 +1,23 @@
+import { Game } from "../scenes/Game";
+import { iterateMap } from "./aux";
+
 describe("aux", () => {
-    test("test", () => {
-        expect(true).toBe(true);
+    let game: Game;
+
+    beforeEach(() => {
+        game = { jett: { life: 0 } } as Game;
+    });
+
+    test("iterate Game Map", () => {
+        const mutateGame = (game: Game) => {
+            game.jett.life++;
+        };
+
+        const { life } = game.jett;
+
+        iterateMap(game, mutateGame, mutateGame);
+
+        expect(game.jett.life).toBe(life + 2);
     });
 });
 
